e260ea17187e8ca3406856520d2408da8186f688
wyao <289672494@qq.com>
2020-09-21 19:55:30 +0800

    [wang yao] 修复async功能不全的问题

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -2,7 +2,7 @@ async function fetchData(url) {
   // <-- start
   const response = await fetch(url);
   const data = await response.json();
-  return data;
+  document.writeln(data.name);
   // end -->
 }
 
