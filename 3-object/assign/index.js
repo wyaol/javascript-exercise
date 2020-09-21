9ab15d920bae15ce0f340506b4725dc4a8d2d550
wyao <289672494@qq.com>
2020-09-21 18:39:03 +0800

    [wang yao] assign finish

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,7 @@
 export default function addSerialNumber(source) {
-  // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  return Object.assign(
+    source,
+    { serialNumber: '12345' },
+    { properties: { color: 'green', status: 'processed' } }
+  );
 }
