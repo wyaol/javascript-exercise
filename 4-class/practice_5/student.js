b314e1ee799825ee22fe88675b12828d77943c79
wyao <289672494@qq.com>
2020-09-21 19:35:33 +0800

    [wang yao] class Teacher finish

--- 4-class/practice_5/student.js
+++ 4-class/practice_5/student.js
@@ -1,1 +1,12 @@
-// TODO 19: 在这里写实现代码
+import Person from './person';
+
+export default class Student extends Person {
+  constructor(name, age, _class) {
+    super(name, age);
+    this.klass = _class;
+  }
+
+  introduce() {
+    return `${super.introduce()} I am a Student. I am at Class ${this.klass}.`;
+  }
+}
--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,15 @@
-// TODO 20: 在这里写实现代码
+import Person from './person';
+
+export default class Teacher extends Person {
+  constructor(name, age, _class) {
+    super(name, age);
+    this.klass = _class;
+  }
+
+  introduce() {
+    if (this.klass === undefined) {
+      return `${super.introduce()} I am a Teacher. I teach No Class.`;
+    }
+    return `${super.introduce()} I am a Teacher. I teach Class ${this.klass}.`;
+  }
+}
