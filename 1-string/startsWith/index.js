8b351dc9bf4206265dd94887ef339d78357be99b
wyao <289672494@qq.com>
2020-09-21 16:46:26 +0800

    [wang yao] 完成startWith

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,7 @@
 export default function collectCarNumberCount(collection) {
-  // TODO 7: 在这里写实现代码
+  const res = collection.reduce((sum, curValue) => {
+    if (curValue.substr(0, 2) === '粤A') return sum + 1;
+    return sum;
+  }, 0);
+  return res;
 }
