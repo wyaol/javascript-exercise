de3c2e07c15012eb4c8db51014d1cf1b3b5b3999
wyao <289672494@qq.com>
2020-09-21 19:40:05 +0800

    [wang yao] xhr finish

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -1,8 +1,12 @@
 function fetchData(url, successCallback, errorCallback) {
   const xhr = new XMLHttpRequest();
   // <-- start
-  // TODO 21: 通过XMLHttpRequest实现异步请求
-
+  xhr.open('GET', url);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState !== 4) return;
+    if (xhr.status === 200) successCallback(xhr.responseText);
+    else errorCallback(xhr.responseText);
+  };
   // end -->
 }
 
