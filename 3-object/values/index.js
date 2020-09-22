03f54a041975c2d48ba77570536803c00204dd12
wyao <289672494@qq.com>
2020-09-22 09:19:39 +0800

    [wang yao] 使用 + 转int

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,3 @@
 export default function countTypesNumber(source) {
-  return Object.values(source).reduce((sum, value) => sum + Number(value), 0);
+  return Object.values(source).reduce((sum, value) => sum + +value, 0);
 }
