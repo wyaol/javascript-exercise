a98fea16b0a2a73d25c96e94301ae74b4aa6faee
wyao <289672494@qq.com>
2020-09-22 09:09:16 +0800

    [wang yao] 使用startsWith替换substr

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,6 +1,6 @@
 export default function collectCarNumberCount(collection) {
   const res = collection.reduce((sum, curValue) => {
-    if (curValue.substr(0, 2) === '粤A') return sum + 1;
+    if (curValue.startsWith('粤A')) return sum + 1;
     return sum;
   }, 0);
   return res;
